9ec8926b62d1847c160404525c0ce64dc24215bf
BruDevin <bruno.ads.2022@gmail.com>
2023-12-26 11:42:22 -0300

    Projeto do curso

---
+++ src/threads/PoolDeConexao.java
@@ -0,0 +1,19 @@
+package threads;
+
+public class PoolDeConexao {
+
+    public String getConnection() {
+
+        System.out.println("Emprestando conexão");
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return "connection";
+
+    }
+
+}
---
+++ src/threads/Principal.java
@@ -0,0 +1,11 @@
+package threads;
+
+public class Principal {
+
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("Thread MAIN");
+
+        Thread.sleep(30000);
+    }
+
+}
